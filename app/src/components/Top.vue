a0c6692613bdf442604ceeb2a12e15ad910cb65b
helloworld753315 <helloworld.cpp.h@gmail.com>
2021-11-04 11:23:52 +0900

    [add] SpotifyAPIの認証部分作成

---
+++ app/src/components/Top.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h1>Topページ</h1>
+    <button @click="spotifyLogin">認証</button>
+    <p>{{ routeParams }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      hoge: "test"
+    }
+  },
+  props: {
+    routeParams: Object
+  },
+  created: function() {
+    if (this.$route.hash) {
+      this.$router.push(this.$route.fullPath.replace('#', '?'))
+    }
+  },
+  methods: {
+    spotifyLogin: function() {
+      let endpoint = 'https://accounts.spotify.com/authorize'
+      let response_type = 'token'
+      let client_id = 'bf090ae6942646348366507a7d6959bc'
+      let redirect_uri = 'http://0.0.0.0:9000'
+      let scope = 'user-read-currently-playing'
+      location.href = endpoint +
+        '?response_type=' + response_type +
+        '&client_id=' + client_id +
+        '&redirect_uri=' + redirect_uri +
+        '&scope=' + scope
+    }
+  }
+}
+</script>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,15 +1,43 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Top from "@/components/Top";
+
+/*
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "Top",
+      component: Top,
+      props: route => {
+        return { routeParams: route.query };
+      }
     }
   ]
-})
+});
+*/
+
+Vue.use(Router);
+
+const routes = [
+  {
+    path: "/",
+    name: "Top",
+    component: Top,
+    props: route => {
+      return { routeParams: route.query }
+    }
+  }
+];
+
+const router = new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
+
