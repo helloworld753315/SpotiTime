c08a9532a9dd0cb3d483282254952d2d1af4580b
helloworld753315 <helloworld.cpp.h@gmail.com>
2021-12-23 11:38:11 +0900

    [add]ボタンアニメーション・装飾追加

--- app/src/components/Top.vue
+++ app/src/components/Top.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="container">
     <h1>SpotiTime</h1>
-    <button @click="spotifyLogin" class="button">認証</button>
-    <button @click="getHistory">取得テスト用</button>
-    <p>合計再生時間: {{MsecondsTominuts(TotalPlayingTime)}} 分</p>
+    <div class="circle">
+      <p>{{MsecondsTominuts(TotalPlayingTime)}} 分</p>
+    </div>
+    <div class="row">
+      <button @click="spotifyLogin" class="button b-small">認証</button>
+      <button @click="getHistory" class="button b-small">見る</button>
+    </div>
+    <!--
     <ul v-for="(value, index) in History">
       <li>{{ConvertJST(value.played_at)}}</li>
       <li>{{value.track.artists[0].name}} / {{value.track.name}}</li>
     </ul>
+    -->
   </div>
 </template>
 
@@ -82,6 +88,7 @@ export default {
     }, 
     getPlayingTime: function(){
       console.log("test")
+      this.TotalPlayingTime = 0
       for (var i = 0, len = this.History.length; i < len; i++) {
         const playing_time = this.History[i].track.duration_ms
         const playing_date = this.History[i].played_at
@@ -106,8 +113,63 @@ export default {
 </script>
 
 <style coped>
+/*
 .button {
+  border: none; 
   background-color: rgb(228, 228, 228);
+  border-radius: 30px;
+}
+*/
+
+.button {
+  width: 140px;
+  height: 45px;
+  font-family: 'Roboto', sans-serif;
+  font-size: 11px;
+  text-transform: uppercase;
+  letter-spacing: 2.5px;
+  font-weight: 500;
+  color: rgb(255, 255, 255);
+  background-color: #191414;
+  border: none;
+  border-radius: 45px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  cursor: pointer;
+  outline: none;
+  }
+
+.button:hover {
+  background-color: #1DB954;
+  box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+  color: #fff;
+  transform: translateY(-2px);
+}
+
+.b-small{
+  width: 100px;
+  height: 40px;
+  margin: 10px;
+}
+
+.b-middle{
+  width: 60px;
+  height: 40px;
+}
+
+.circle {
+    margin:0 auto;
+    width: 140px;
+    height: 140px; 
+    background-color: #a5a5a5;
+    border-radius: 50%;/* ←円を作る */
+    line-height: 140px;
+    color: rgb(255, 255, 255);
+}
+
+.row{
+  margin: 0 auto;
+  padding: 30px 20px;
 }
 </style>
 
